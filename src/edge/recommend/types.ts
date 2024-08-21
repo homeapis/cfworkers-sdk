6a2c1ccdb02995ddd5292a8f215c5d44f8d63476
theoctt <130503991+theoctt@users.noreply.github.com>
2024-08-21 22:44:55 +0200

    removing cached Docs

--- src/edge/recommend/types.ts
+++
@@ -1,7 +0,0 @@
-export interface GuestLink {
-  path: string;
-  url: string;
-  created_at: number;
-  updated_at: number;
-  _id: string;
-}
--- src/schemas/subscriber.ts
+++
@@ -1,7 +0,0 @@
-import { ISOString } from './countriesIso';
-
-interface Subscriber {
-  country: ISOString;
-}
-
-export type { Subscriber as WebSubscriber };
